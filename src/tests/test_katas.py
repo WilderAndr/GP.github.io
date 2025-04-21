0882710160e7168bf1758dd523869e6e1a88b6e2
Wilder-Arredondo <48569015@continental.edu.pe>
2025-04-20 20:31:04 -0500

    Create test_katas.py

---
+++ src/tests/test_katas.py
@@ -0,0 +1,13 @@
+from katas.string_calculator import add
+
+def test_add_empty_string():
+    assert add("") == 0
+
+def test_add_single_number():
+    assert add("1") == 1
+
+def test_add_two_numbers():
+    assert add("1,2") == 3
+
+def test_add_multiple_numbers():
+    assert add("1,2,3,4,5") == 15
